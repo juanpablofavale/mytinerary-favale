93de10d964598093df1fa9e2a10a7aaf1a67ba0b
jpf <juanpablofavale@hotmail.com>
2023-08-08 21:16:24 -0300

    agregando imagen al nav

--- src/components/NavBar/NavBarMain.jsx
+++ src/components/NavBar/NavBarMain.jsx
@@ -4,6 +4,7 @@ export default function NavBar({links}) {
     return (
         <nav>
             {links.map((link, index) => <Anchor key={index} title={link.title} link={link.link} />)}
+            <img src="./usrDef.png" alt="Default User" />
         </nav>
     )
 }
