19890096ee27cccbf67e74dce3ed4b4456321d18
jpf <juanpablofavale@hotmail.com>
2023-08-08 21:50:55 -0300

    preparando componente cities

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import Home from './pages/Home/Home'
+import Cities from './pages/Cities/Cities'
 
 function App() {
   return (
     <>
       <Home />
+      {/*
+        <Cities />
+      */}
     </>
   )
 }
--- src/pages/Cities/Cities.jsx
+++ src/pages/Cities/Cities.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import LayoutMain from '../LayoutMain/LayoutMain'
+import './cities.css'
 
 export default function Cities() {
   return (
-    <div>Cities</div>
+    <LayoutMain>
+      <main className='pgCities'>
+        <div className='hero'>
+          <h2>Cities</h2>
+        </div>
+      </main>
+    </LayoutMain>
   )
 }
