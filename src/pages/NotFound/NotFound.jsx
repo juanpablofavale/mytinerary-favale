59f9c0a8fe785c107d4bed7ab7303cb4d38ee865
jpf <juanpablofavale@hotmail.com>
2023-08-09 19:32:39 -0300

    estilos al 404

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -5,7 +5,8 @@ export default function NotFound() {
   return (
     <main className='pg404'>
         <div className='hero'>
-            <h2>404 - Page Not Found</h2>
+            <img src="../404.png" alt="404" />
+            <h2>Page Not Found</h2>
         </div>
     </main>
   )
