1fb5d9d3cd8976797517128346f66f9d5554828a
jpf <juanpablofavale@hotmail.com>
2023-09-08 02:56:14 -0300

    tratando array de paises

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,11 +1,11 @@
-import { useEffect, useRef } from 'react'
+import { useRef } from 'react'
 import './signUp.css'
 import { useDispatch, useSelector } from 'react-redux'
-import { store } from '../../redux/store'
 import { signUpAsync } from '../../redux/actions/authActions'
 import { Link } from 'react-router-dom'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 import Google from '../Google/Google'
+import * as locale from 'locale-codes';
 
 export default function Signup(){
     const {user, token, message} = useSelector(store => store.authReducer)
@@ -39,6 +39,17 @@ export default function Signup(){
         e.preventDefault()
     }
 
+    const getCountry = () => {
+        const filt = locale.all.filter(loc => loc.location)
+        const aux = filt.map(loc => loc.location)
+        const unicos = aux.filter((l, i) => aux.indexOf(l) === i)
+        return unicos
+    }
+
+    const selected = (name) => {
+        name=="Argentina" ? "selected" : ""
+    }
+
     return (
         <div className="pgSignUp">
             <div className="card">
@@ -58,8 +69,8 @@ export default function Signup(){
                         <label htmlFor="imgProfile">Profile Image:</label>
                         <input ref={image} type="text" id="imgProfile" required/>
                         <label htmlFor="country">Country:
-                        <select ref={country} name="" id="country">
-                            <option value="Argentina">Argentina</option>
+                        <select ref={country} name="" id="country" defaultValue="Argentina">
+                            {getCountry().map((loc, i) => <option key={i} value={loc}>{loc}</option>)}
                         </select>
                         </label>
                         <input onClick={handleClick} type="submit" value="Login" />
