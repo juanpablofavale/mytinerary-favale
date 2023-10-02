9db60c16ead81948362b11ec4942451214a441e6
jpf <juanpablofavale@hotmail.com>
2023-10-02 14:59:28 -0300

    cambiando nombres de importaciones

--- src/components/NavBar/NavBarMain.jsx
+++ src/components/NavBar/NavBarMain.jsx
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from "react-redux"
 import { Link, useNavigate } from "react-router-dom"
 import { signOutAsync, signInAsyncToken } from "../../redux/actions/authActions"
 import { useEffect } from "react"
-import './navbarMain.css'
+import './navbarmain.css'
 import BasicNav from "../BasicNav/BasicNav"
 import { toast } from "react-toastify"
 
